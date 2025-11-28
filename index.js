2e814629c487fe23670ad47528544ef6838ad786
susmitaomi991 <susmitaaislamomi@gmail.com>
2025-11-28 11:52:58 +0600

    love btn count

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ let count = 0;
 
 
 const loveButtons = document.querySelectorAll(".love-btn");
-
 loveButtons.forEach((btn) => {
   btn.addEventListener("click", function () {
     count++;
     loveCount.textContent = count;
   });
 });
-
 
 
 
